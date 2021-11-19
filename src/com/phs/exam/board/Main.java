d3678f74cdce83b05a86dcf3ef55e159fe605167
solsolsol11 <ushio941@gmail.com>
2021-11-19 19:52:50 +0900

    exit입력 시 명령어 계속 입력 받기

--- src/com/phs/exam/board/Main.java
+++ src/com/phs/exam/board/Main.java
@@ -4,7 +4,7 @@ package com.phs.exam.board;
 ### TODO 리스트
 * [x]시작 알림 메세지 출력
 * [x]명령어 입력
-* [ ]exit가 입력될 떄 까지 명령어 계속 입력받기
+* [x]exit가 입력될 떄 까지 명령어 계속 입력받기
 * [ ]새 게시물 입력받기 기능 구현
 * [ ]등록시마다 게시물번호가 증가하도록
 * [ ]입력받은 게시물 정보를 Article 객체에 저장 후 객체변수 출력
@@ -28,9 +28,21 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령 : ) ");
-    String od = sc.nextLine();
+
+    while(true){
+
+
+      System.out.printf("명령 : ) ");
+      String cmd = sc.nextLine();
+      if(cmd.equals("exit")){
+        break;
+      }
+
+
+    }
+    sc.close();
     System.out.println("== 프로그램 종료 ==");
-    System.out.println("== 프로그램 종료 ==");
+
+
   }
 }
